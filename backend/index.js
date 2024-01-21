71c5d851db7a7c1cfe2e31883a096d83ab25537c
Shubrajit22 <138674279+Shubrajit22@users.noreply.github.com>
2024-01-21 13:52:27 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express =vrequire("express");
 const users = require('./MOCK_DATA.json')
 const app=express();
+const fs= require("fs")
 
 const PORT=3000;
 app.get('/users',(req,res)=>{
@@ -16,17 +17,31 @@ app.get('/users',(req,res)=>{
 // })
 // baar baar ek hi route ko na likhkaar ek hi baar likho or usme handlers chala do jese
 app.route('/users/:id')
+
 .get((req,res)=> {const id = req.params.id;
 const user=users.find((user)=>user.id=== id);
 return res.json(user);})
+
 .patch((req,res)=>{
     // edit user with id
     return res.json({status:'status pending'})
+})
+
+.post((req,res)=>{
+  const body=req.body; 
+  //req.body => jo bhi form ya front end se data aaayega req.body me available rahega
+  users.push({...body, id: users.length + 1}); 
+  //jo bhi variable body me content hai usko humne data set/json data usme dhal dia
+  fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+    return res.json({status:`success`, id: users.length })
+
+  })
 })
 .delete((req,res)=>{
   // delete user with user id
   return res.json({status:`pending`})
 })
+
 app.listen(PORT,()=>{
     console.log(`server started!!! : http://localhost:3000`)
 })
